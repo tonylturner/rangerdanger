d4014f01fea17bd68e978d89ad6b9710d9aeac6d
tonylturner <tony@sentinel24.com>
2026-04-07 18:34:36 -0400

    temp hmi

---
+++ frontend/app/hmi/page.tsx
@@ -0,0 +1,9 @@
+import { AdvancedHmi } from "../../components/advanced-hmi";
+
+export default function HmiPage() {
+  return (
+    <main className="flex h-[calc(100vh-0px)] flex-col overflow-hidden">
+      <AdvancedHmi />
+    </main>
+  );
+}
